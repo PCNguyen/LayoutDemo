3b29fd67309b535469318e67be70db1c4e0bb24b
PC <pc@macbooks-MacBook-Air-2.local>
2015-02-12 22:20:53 -0800

    layout collection view

--- LayoutDemo/LDPhotoViewController.m
+++ LayoutDemo/LDPhotoViewController.m
@@ -8,22 +8,89 @@
 
 #import "LDPhotoViewController.h"
 
-/*******************************
- 
+/********************************
+ LDPhotoCell
  *******************************/
 
-@interface LDPhotoViewController ()
+@interface LDPhotoCell : UICollectionViewCell
+
+@end
+
+@implementation LDPhotoCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    self.contentView.backgroundColor = [UIColor lightGrayColor];
+  }
+  
+  return self;
+}
+
+@end
+
+/*******************************
+ LDPhotoViewController
+ *******************************/
+
+#define LDPhotoViewControllerCellSize       CGSizeMake(50.0f, 50.0f)
+
+static NSString *const LDPhotoViewControllerCellIdentifier = @"LDPhotoViewControllerCellIdentifier";
+static NSInteger const LDPhotoViewControllerItemCount = 120;
+
+@interface LDPhotoViewController () <UICollectionViewDelegateFlowLayout, UICollectionViewDataSource>
+
+@property (strong, nonatomic) UICollectionView *photoCollectionView;
 
 @end
 
 @implementation LDPhotoViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+#pragma mark - View Life Cycle
+
+- (void)loadView
+{
+  [super loadView];
+  
+  [self.view addSubview:self.photoCollectionView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+- (void)viewWillLayoutSubviews
+{
+  [super viewWillLayoutSubviews];
+  self.photoCollectionView.frame = self.view.bounds;
+}
+
+#pragma mark - Collection View
+
+- (UICollectionView *)photoCollectionView
+{
+  if (!_photoCollectionView) {
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.itemSize = LDPhotoViewControllerCellSize;
+    
+    _photoCollectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flowLayout];
+    _photoCollectionView.delegate = self;
+    _photoCollectionView.dataSource = self;
+    _photoCollectionView.backgroundColor = [UIColor clearColor];
+    [_photoCollectionView registerClass:[LDPhotoCell class] forCellWithReuseIdentifier:LDPhotoViewControllerCellIdentifier];
+  }
+  
+  return _photoCollectionView;
+}
+
+#pragma mark - Collection View Delegate / DataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+  return LDPhotoViewControllerItemCount;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+  LDPhotoCell *photoCell = [collectionView dequeueReusableCellWithReuseIdentifier:LDPhotoViewControllerCellIdentifier forIndexPath:indexPath];
+  
+  return photoCell;
 }
 
 @end
