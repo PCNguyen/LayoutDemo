f03aacdb83136e4092a1ba4817f1976a7d0500d8
PC <pc@macbooks-MacBook-Air-2.local>
2015-02-12 21:17:41 -0800

    Initial Commit

---
+++ LayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutDemo
+//
+//  Created by PC on 2/12/15.
+//  Copyright (c) 2015 PC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutDemo
+//
+//  Created by PC on 2/12/15.
+//  Copyright (c) 2015 PC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
